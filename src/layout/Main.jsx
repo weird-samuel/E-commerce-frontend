76f321c865c3e962a879b0579a529b65e5a598e4
weird-samuel <fiyinfoluwasamuel580@gmail.com>
2024-01-02 02:43:59 +0100

    router setup

---
+++ src/layout/Main.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+const Main = () => {
+  return (
+    <div>
+      <nav>Navbar</nav>
+      <Outlet />
+      <footer>Footer</footer>
+    </div>
+  );
+};
+
+export default Main;
