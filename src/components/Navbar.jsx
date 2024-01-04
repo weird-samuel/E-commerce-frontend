ae05d0fc0d9b0933f7c1130fc796ca0ea71fc08c
weird-samuel <fiyinfoluwasamuel580@gmail.com>
2024-01-04 14:52:41 +0100

    ...

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,30 @@
 const Navbar = () => {
+  const navItems = (
+    <>
+      <li>
+        <a>Item 1</a>
+      </li>
+      <li>
+        <details>
+          <summary>Parent</summary>
+          <ul className="p-2">
+            <li>
+              <a>Submenu 1</a>
+            </li>
+            <li>
+              <a>Submenu 2</a>
+            </li>
+          </ul>
+        </details>
+      </li>
+      <li>
+        <a>Item 3</a>
+      </li>
+    </>
+  );
   return (
-    <header>
-      <div className="navbar bg-base-100">
+    <header className="max-w-screen-2xl container mx-auto">
+      <div className="navbar xl:px-24">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -46,27 +69,7 @@ const Navbar = () => {
           <a className="btn btn-ghost text-xl">Logo</a>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
+          <ul className="menu menu-horizontal px-1">{/* nav item */}</ul>
         </div>
         <div className="navbar-end">
           <a className="btn">Button</a>
