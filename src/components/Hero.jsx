51e7a085f6b4fcc74f4377ec8c75f6b52ac52328
weird-samuel <fiyinfoluwasamuel580@gmail.com>
2024-01-06 22:13:44 +0100

    4 hours in the mud, i did 2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,120 @@
+import Banner from "/images/home/Banner.png";
+import GenericLp from "/images/home/generic-lp.jpg";
+import OfficeLp from "/images/home/office-lp.jpg";
 //
 const Hero = () => {
   return (
     <div className="section-container">
-      <div className="py-24 flex flex-col md:flex-row justify-between items-center gap-8">
+      <div className="py-24 flex flex-col md:flex-row-reverse justify-between items-center gap-8">
+        <div className="md:w-1/2">
+          <img src={Banner} alt="Banner-image" />
+          <div className="hidden lg:flex flex-col md:flex-row items-center justify-around -mt-14 gap-4">
+            <div>
+              <div className="flex bg-white py-2 px-3 rounded-2xl items-center gap-3 shadow-md w-64">
+                <div className="w-20 h-20 rounded-2xl overflow-hidden">
+                  <img
+                    src={GenericLp}
+                    alt="generic laptop"
+                    className="object-cover w-full h-full"
+                  />
+                </div>
+                <div className="space-y-1">
+                  <h5 className="font-medium mb-1 text-black">
+                    Generic Laptop
+                  </h5>
+                  <div className="rating rating-sm">
+                    <input
+                      type="radio"
+                      name="rating-1"
+                      className="mask mask-star bg-green"
+                    />
+                    <input
+                      type="radio"
+                      name="rating-1"
+                      className="mask mask-star bg-green"
+                      defaultChecked
+                    />
+                    <input
+                      type="radio"
+                      name="rating-1"
+                      className="mask mask-star bg-green"
+                      defaultChecked
+                    />
+                    <input
+                      type="radio"
+                      name="rating-1"
+                      className="mask mask-star bg-green"
+                      defaultChecked
+                    />
+                    <input
+                      type="radio"
+                      name="rating-1"
+                      className="mask mask-star bg-green"
+                    />
+                  </div>
+                  <p className="text-black">$80.00</p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="flex bg-white py-2 px-3 rounded-2xl items-center gap-3 shadow-md w-64">
+                <div className="w-20 h-20 rounded-2xl overflow-hidden">
+                  <img
+                    src={OfficeLp}
+                    alt="Office laptop"
+                    className="object-cover w-full h-full"
+                  />
+                </div>
+                <div className="space-y-1">
+                  <h5 className="font-medium mb-1 text-black">Office Laptop</h5>
+                  <div className="rating rating-sm">
+                    <input
+                      type="radio"
+                      name="rating-1"
+                      className="mask mask-star bg-green"
+                    />
+                    <input
+                      type="radio"
+                      name="rating-1"
+                      className="mask mask-star bg-green"
+                      defaultChecked
+                    />
+                    <input
+                      type="radio"
+                      name="rating-1"
+                      className="mask mask-star bg-green"
+                      defaultChecked
+                    />
+                    <input
+                      type="radio"
+                      name="rating-1"
+                      className="mask mask-star bg-green"
+                      defaultChecked
+                    />
+                    <input
+                      type="radio"
+                      name="rating-1"
+                      className="mask mask-star bg-green"
+                    />
+                  </div>
+                  <p className="text-black">$80.00</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="md:w-1/2 space-y-7 px-4">
           <h2 className="md:text-5xl text-4xl font-bold leading-snug">
             Explore the World of Great
             <span className="text-green"> Laptops</span>
           </h2>
           <p className="text-xl text-gray-100">
-            Embark on a Journey of Laptop Excellence and Passionate
-            Craftsmanship.
+            Embark on a Journey of Laptop Excellence and Make Compuation Easy
+            for you!
           </p>
           <button className="btn bg-green border-none outline-none rounded-full text-white px-8 py-3 font-semibold">
             Order Now
           </button>
-        </div>
-        <div className="md:w-1/2">
-          <img src="" alt="" />
         </div>
       </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from "react";
 import { BiPhoneCall } from "react-icons/bi";
 
 const Navbar = () => {
+  const [isSticky, setSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const offset = window.scrollY;
+      if (offset > 0) {
+        setSticky(true);
+      } else {
+        setSticky(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   const navItems = (
     <>
       <li>
@@ -44,8 +61,14 @@ const Navbar = () => {
     </>
   );
   return (
-    <header className="max-w-screen-2xl container mx-auto">
-      <div className="navbar xl:px-24">
+    <header className="max-w-screen-2xl container mx-auto fixed top-0 left-0 right-0 transition-all duration-500 ease-in-out">
+      <div
+        className={`navbar xl:px-24 ${
+          isSticky
+            ? "shadow-md bg-base-100 transition-all duration-500 ease-in-out"
+            : ""
+        }`}
+      >
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
